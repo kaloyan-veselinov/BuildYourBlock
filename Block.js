4f50e6e0ae0b8f8030338eb918c5b59acdd9df5a
Kaloyan Veselinov <kvesselinov@gmail.com>
2019-03-08 15:14:12 +0100

    MAJ partie 2

--- Block.js
+++ Block.js
@@ -1,8 +1,13 @@
 const { BlockTool } = require('./tools');
+const crypto = require('crypto');
 
 // Je mets ça là ... au cas où ...
 function generateId() {
   return Math.floor(Math.random()*1000000000);
+}
+
+function hash(data) {
+  return crypto.createHash('sha256').update(data, 'utf8').digest('hex');
 }
 
 // Vous n'avez pas à comprendre BlockTool.
@@ -16,19 +21,33 @@ module.exports = class Block extends BlockTool {
     super() // Obligatoire car on hérite de BlockTool
     // Le mot clé `this` permet d'accèder aux propriétés de l'object depuis ses méthodes.
     this.previous = previous;
-    //...
+    this.data = data;
+    this.date = new Date();
+    this.id = this.getHash();
+    this.nonce = 0;
   }
 
+  stringify(){
+    return this.data.concat(this.date, this.previous, this.nonce);
+  }
   // Retourne l'identifiant du block en le calculant depuis les données
-  getHash() {}
+  getHash() {
+    return hash(this.stringify());
+  }
 
   // Retourne un boolean qui indique si le block est valide
   isValid(
     DIFFICULTY // Utile à l'étape 2
   ) {
-    return true;
+    let prefix = "0".repeat(DIFFICULTY);
+    return this.id === this.getHash() && this.id.startsWith(prefix);
   }
 
   // Utile à l'étape 2
-  miner(DIFFICULTY) {}
+  miner(DIFFICULTY) {
+    while(!this.isValid(DIFFICULTY)){
+      this.nonce++;
+      this.id = this.getHash();
+    }
+  }
 }
