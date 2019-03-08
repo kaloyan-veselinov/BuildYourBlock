d4c9c322be892848688ee1c4740ef07583645136
Kaloyan Veselinov <kvesselinov@gmail.com>
2019-03-08 15:29:04 +0100

    RSA OK

--- RSATools.js
+++ RSATools.js
@@ -4,19 +4,22 @@ module.exports = class RSATools {
 
   // Retourne la clé publique associée à la clé privée
   static privateToPublic(privateKeyString) {
-    return ; // ...
+    let privateKey = new NodeRSA(privateKeyString);
+    return privateKey.exportKey("public");
   }
 
   // Retourne la signature du message par la clé privée
   // https://github.com/rzcoder/node-rsa#signingverifying
   static sign(msg, privateKeyString) {
-    return ; // ...
+    let privateKey = new NodeRSA(privateKeyString);
+    return privateKey.sign(msg);
   }
 
   // Vérifie la signature du message par la clé publique
   // Retourne un booléen à true si la signature est bonne
   // https://github.com/rzcoder/node-rsa#signingverifying
   static verify(msg, signature, publicKeyString) {
-    return true; // ...
+    let publicKey = new NodeRSA(publicKeyString);
+    return publicKey.verify(msg, signature); 
   }
 }
